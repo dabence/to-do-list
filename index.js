2bca00cbbe7c4952df945d5351d5dc8cee47eac6
dabence <89123135+dabence@users.noreply.github.com>
2023-09-18 10:06:58 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+import bodyParser from "body-parser";
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+const options = {  year: 'numeric', month: 'short', day: 'numeric' };
+var currentDate =  new Date().toLocaleDateString('en-US', options);
+
+//create arrays for tasks
+var todayList = [];
+var workList = [];
+
+//add function to add the notes to the array
+function addNotes(list, noteToBeAdded){
+    list.push(noteToBeAdded);
+}
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.listen(port, ()=>{
+    console.log(`Running at port ${port}`);
+})
+
+app.get("/", (req, res) =>{
+    res.render("index.ejs", {currentDate: currentDate, todayList:todayList});
+
+});
+
+app.get("/worklist", (req, res) =>{
+    res.render("work-list.ejs", {workList: workList});
+
+});
+
+//submit req from today list
+app.post("/submit", (req, res)=>{
+   
+    addNotes(todayList, req.body["to-do"]);
+    res.render("index.ejs", {currentDate: currentDate, todayList : todayList});
+});
+
+//submit req from work list
+
+app.post("/submitwork", (req, res)=>{ 
+    addNotes(workList, req.body["work-to-do"]);
+    res.render("work-list.ejs", {workList : workList});
+    
+});
+
+
+
+
+
+
